b0cfec5ab8b3898695aeed8a41717d18abff8423
Sebastian Radloff <sradloff23@gmail.com>
2016-05-28 23:45:39 -0400

    configured port to be dynamically added.

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 let app = express();
 let router = express.Router();
+let port = process.env.PORT || 8080;
 
 /* serve dist which contains bundled resources */
 let distDir = path.join(__dirname, 'dist/');
@@ -13,7 +14,7 @@ app.get('*', (request, response) => {
   response.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(8080, () => {
+app.listen(port, () => {
   console.log('Starting App.');
-  console.log('Magic happens on port 8080!');
+  console.log('Magic happens on port ' + port + '!');
 });
