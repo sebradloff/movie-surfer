3077c544c095aeb1a0a1ec9ad346c27faab3f445
sebradloff <sradloff23@gmail.com>
2016-08-13 15:42:12 -0400

    added initial load of discover movies data.

--- src/discover/DiscoverContainer.jsx
+++ src/discover/DiscoverContainer.jsx
@@ -1,7 +1,39 @@
 import React from 'react';
-
+import MovieApi from './movieApi';
 
 export default class DiscoverContainer extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = this.initialState();
+
+    this.movieApi = new MovieApi();
+    this.successCallback = this.successCallback.bind(this);
+    this.errorCallback = this.errorCallback.bind(this);
+  }
+
+  componentDidMount() {
+    this.movieApi.discover(this.successCallback, this.errorCallback);
+  }
+
+  initialState() {
+    return {
+      movieData: {},
+      error: null
+    };
+  }
+
+  successCallback(movies) {
+    this.setState({
+      movieData: movies
+    });
+  }
+
+  errorCallback(error) {
+    this.setState({
+      error
+    });
+  }
 
   render() {
     return (
