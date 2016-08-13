4cb2523c3b44f696e8ecb39d7a83adf3ad156fa3
sebradloff <sradloff23@gmail.com>
2016-08-13 14:10:47 -0400

    added DiscoverContainer as the index route of the app.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, browserHistory } from 'react-router';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 import App from './App';
+import DiscoverContainer from './discover/DiscoverContainer';
 
 ReactDOM.render((
   <Router history={browserHistory}>
-    <Route path="/" component={App} />
+    <Route path="/" component={App}>
+      <IndexRoute component={DiscoverContainer} />
+    </Route>
   </Router>
 ), document.getElementById('App'));
