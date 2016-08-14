4c95fa465b40e836a1c930a70559de66037746a7
sebradloff <sradloff23@gmail.com>
2016-08-13 20:46:03 -0400

    loading images over https instead of http.

--- src/movie/MovieCard.jsx
+++ src/movie/MovieCard.jsx
@@ -9,7 +9,7 @@ const MovieCard = ({ info }) => {
   return (
     <div className="six wide column">
       <h1 className="ui header">{info.original_title}</h1>
-      <img alt={`${info.original_title} poster`} src={`http://image.tmdb.org/t/p/w342${info.poster_path}`}></img>
+      <img alt={`${info.original_title} poster`} src={`https://image.tmdb.org/t/p/w342${info.poster_path}`}></img>
       <Rating rating={info.vote_average} />
       <div>{info.overview}</div>
     </div>
