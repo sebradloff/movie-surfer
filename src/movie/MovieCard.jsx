e4bf9db418dbfde7984d6982ed38f44a5f727d9e
sebradloff <sradloff23@gmail.com>
2016-08-23 19:07:26 -0400

    added movie-card class which styles each MovieCard element.

--- src/movie/MovieCard.jsx
+++ src/movie/MovieCard.jsx
@@ -8,10 +8,12 @@ const propTypes = {
 const MovieCard = ({ info }) => {
   return (
     <div className="eight wide column">
-      <h1 className="ui header">{info.original_title}</h1>
-      <img alt={`${info.original_title} poster`} src={`https://image.tmdb.org/t/p/w342${info.poster_path}`}></img>
-      <Rating rating={info.vote_average} />
-      <div>{info.overview}</div>
+      <div className="movie-card">
+        <h1 className="ui header">{info.original_title}</h1>
+        <img alt={`${info.original_title} poster`} src={`https://image.tmdb.org/t/p/w342${info.poster_path}`}></img>
+        <Rating rating={info.vote_average} />
+        <div>{info.overview}</div>
+      </div>
     </div>
   );
 };
