e04bcba07058a486fe9ebc1042386cef3084cca3
sebradloff <sradloff23@gmail.com>
2016-10-31 15:29:25 +0100

    added start of DetailedMovieCard split into left and right portions of the screen.

--- src/movie/DetailedMovieCard.jsx
+++ src/movie/DetailedMovieCard.jsx
@@ -8,12 +8,16 @@ const propTypes = {
 const DetailedMovieCard = ({ info }) => {
   return (
     <div className="eight wide column">
-      <div className="movie-card">
-        <h1 className="ui header">{info.original_title}</h1>
-        <img alt={`${info.original_title} poster`} src={`https://image.tmdb.org/t/p/w342${info.poster_path}`}></img>
-        <div className="tagline">{info.tagline}</div>
-        <Rating rating={info.vote_average} />
-        <div>{info.overview}</div>
+      <div id="detailed-movie-card" className="ui grid">
+        <div id="left-side" className="six wide column">
+          <h1 className="ui header">{info.original_title}</h1>
+          <img alt={`${info.original_title} poster`} src={`https://image.tmdb.org/t/p/w342${info.poster_path}`}></img>
+          <div className="tagline">{info.tagline}</div>
+          <Rating rating={info.vote_average} />
+        </div>
+        <div id="right-side" className="ten wide column">
+          <div>{info.overview}</div>
+        </div>
       </div>
     </div>
   );
