1fdf57e1df3503b1b407c4e31e3889ed9cc8d6e1
sebradloff <sradloff23@gmail.com>
2016-11-04 17:05:21 -0400

    moved the rating beneath the title for a MovieCard.

--- src/movie/MovieCard.jsx
+++ src/movie/MovieCard.jsx
@@ -10,8 +10,8 @@ const MovieCard = ({ info }) => {
     <div className="eight wide column">
       <div className="movie-card">
         <h1 className="ui header">{info.original_title}</h1>
-        <img alt={`${info.original_title} poster`} src={`https://image.tmdb.org/t/p/w342${info.poster_path}`}></img>
         <Rating rating={info.vote_average} />
+        <img alt={`${info.original_title} poster`} src={`https://image.tmdb.org/t/p/w342${info.poster_path}`}></img>
         <div>{info.overview}</div>
       </div>
     </div>
