021c2474ff76795f5c5e167ee28ed25ed5e6b925
sebradloff <sradloff23@gmail.com>
2016-11-04 17:39:48 -0400

    added links for each MovieCard so it bring you to the detailed movie page.

--- src/movie/MovieCard.jsx
+++ src/movie/MovieCard.jsx
@@ -8,12 +8,14 @@ const propTypes = {
 const MovieCard = ({ info }) => {
   return (
     <div className="eight wide column">
-      <div className="movie-card">
-        <h1 className="ui header">{info.original_title}</h1>
-        <Rating rating={info.vote_average} />
-        <img alt={`${info.original_title} poster`} src={`https://image.tmdb.org/t/p/w342${info.poster_path}`}></img>
-        <div>{info.overview}</div>
-      </div>
+      <a className="movie-link" href={`/movies/${info.id}`}>
+        <div className="movie-card">
+          <h1 className="ui header">{info.original_title}</h1>
+          <Rating rating={info.vote_average} />
+          <img alt={`${info.original_title} poster`} src={`https://image.tmdb.org/t/p/w342${info.poster_path}`}></img>
+          <div>{info.overview}</div>
+        </div>
+      </a>
     </div>
   );
 };
