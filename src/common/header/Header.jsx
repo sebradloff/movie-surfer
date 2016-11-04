f46f5a386ecfad1ef618d8d91b651ad58f4352ad
sebradloff <sradloff23@gmail.com>
2016-11-04 16:42:27 -0400

    made Movie Surfer header clickable to go back to homepage.

--- src/common/header/Header.jsx
+++ src/common/header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     <div id="header" className="ui two column grid">
       <div className="row">
         <div className="three wide column"></div>
-        <h1 className="ten wide column" style={{ textAlign: 'center' }}>Movie Surfer</h1>
+        <h1 className="ten wide column" style={{ textAlign: 'center' }}><a href="/">Movie Surfer</a></h1>
         <div className="right floated three wide column">
           <SearchBar />
         </div>
