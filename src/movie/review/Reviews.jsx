4f874ad521ec5a7eef962ccb57000ed44be2a384
sebradloff <sradloff23@gmail.com>
2016-11-05 16:03:24 -0400

    added messaging for when there are no reviews.

--- src/movie/review/Reviews.jsx
+++ src/movie/review/Reviews.jsx
@@ -7,12 +7,20 @@ const propTypes = {
 };
 
 const Reviews = ({ reviews }) => {
+  let reviewsJsx;
+  if (reviews.length !== 0) {
+    reviewsJsx = reviews.map((review) => {
+      return <Review key={review.id} info={review} />;
+    });
+  } else {
+    reviewsJsx = (<div className="ui negative message">
+      <h2 className="ui header">SORRY, but there are no reviews for this movie yet!</h2>
+    </div>);
+  }
   return (
     <div id="reviews">
       <div className="ui blue ribbon label">Reviews</div>
-      {reviews.map((review) => {
-        return <Review key={review.id} info={review} />;
-      })}
+      {reviewsJsx}
     </div>
   );
 };
