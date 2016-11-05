10196c67c2ae6adfebdd54f74d7db1a4c0e35c98
sebradloff <sradloff23@gmail.com>
2016-11-05 13:11:13 -0400

    removed black borders around DetailedMovieCard and added teal borders for both left and right sections.

--- src/movie/DetailedMovieCard.jsx
+++ src/movie/DetailedMovieCard.jsx
@@ -9,11 +9,13 @@ const propTypes = {
 const DetailedMovieCard = ({ info }) => {
   return (
     <div id="detailed-movie-card" className="ui padded grid">
-      <div id="left-side" className="six wide column">
-        <h1 className="ui header">{info.original_title}</h1>
-        <Rating rating={info.vote_average} />
-        <img alt={`${info.original_title} poster`} src={`https://image.tmdb.org/t/p/w342${info.poster_path}`}></img>
-        <div className="tagline">{info.tagline}</div>
+      <div className="six wide column">
+        <div id="left-side">
+          <h1 className="ui header">{info.original_title}</h1>
+          <Rating rating={info.vote_average} />
+          <img alt={`${info.original_title} poster`} src={`https://image.tmdb.org/t/p/w342${info.poster_path}`}></img>
+          <div className="tagline">{info.tagline}</div>
+        </div>
       </div>
       <div id="right-side" className="ten wide column">
         <MovieInfo info={info} />
